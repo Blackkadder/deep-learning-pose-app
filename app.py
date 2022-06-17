2b5835c62173a5ba8fc93e5048fccd8c0b455b50
Rob Bajracharya <rb10963@jetblue.com>
2022-06-18 01:12:44 +0545

    added pic

--- app.py
+++ app.py
@@ -2,17 +2,25 @@
 import streamlit as st
 import seaborn as sns
 
-# page config
-st.set_page_config(page_icon="📈", page_title="App", layout = 'wide')
 
-# load sample dataset
-iris = sns.load_dataset('iris')
+def main():
+    # page config
+    st.set_page_config(page_icon="📈", page_title="App", layout = 'wide')
+
+    # load sample dataset
+    iris = sns.load_dataset('iris')
 
 
-# Heading
-st.markdown('# HELLO WOrLD!')
+    # Heading
+    st.markdown('# HELLO WOrLD!')
+    st.markdown('**The** quick brown *fox* runs over the lazy dog.')
+    st.image('https://c.ndtvimg.com//yoga_625x300_1529482160763.jpg')
+    # body
+    st.dataframe(iris)
 
-# body
-st.dataframe(iris)
+
+if __name__ == "__main__":
+   
+    main()
 
 
