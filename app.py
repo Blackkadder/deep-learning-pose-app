a34394f378f4e2f2f3412e1209b51cabb1265de2
Rob Bajracharya <rb10963@jetblue.com>
2022-06-18 00:17:20 +0545

    added dataset

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
+# libraries
 import streamlit as st
+import seaborn as sns
 
+# page config
 st.set_page_config(page_icon="📈", page_title="App", layout = 'wide')
 
-st.markdown('#HELLO WOrLD!')
-    
+# load sample dataset
+iris = sns.load_dataset('iris')
+
+
+# Heading
+st.markdown('# HELLO WOrLD!')
+
+# body
+st.dataframe(iris)
+
 
